611e05610ace234a3661df1be03002f86248503a
VKach-dev <vkach@zera.vision>
2024-04-05 14:34:04 +0300

    Finished adaptive White label solution. and Parking ticket.

--- src/app/[lang]/contact/page.jsx
+++ src/app/[lang]/contact/page.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { HowItWorksSection } from '@/app/[lang]/home/components/sections/components/fuelFroundSections/howItWorksSection';
 import { Container } from '@/components/container/container';
 
 const ContactPage = () => {
   return (
     <Container>
-      <div className='my-20'>
-        <HowItWorksSection />
-      </div>
+      <div className='my-20'></div>
     </Container>
   );
 };
