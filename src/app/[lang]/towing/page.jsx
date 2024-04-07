b49951b040dad01f0c3fb3b3ec84b87bb734eb1d
VKach-dev <vkach@zera.vision>
2024-04-07 08:37:06 +0300

    Merged

--- src/app/[lang]/towing/page.jsx
+++ src/app/[lang]/towing/page.jsx
@@ -26,22 +26,21 @@ const TowingPage = () => {
       <div className='mt-[170px] max-md:mt-[100px]'>
         <Uber />
       </div>
-        <FullControl
-          smallCover={FullControlTowingMobile}
-          cover={FullControlTowing}
-          subtitle='Over the app and dashboard you can
+      <FullControl
+        smallCover={FullControlTowingMobile}
+        cover={FullControlTowing}
+        subtitle='Over the app and dashboard you can
         order nearest Towing car free of charge. And
         you can see all details in real time.'
-        />
-        <FullControl
-          smallCover={FullControlFraudMobile}
-          cover={FullControlFraud}
-          subtitle='In the dashboard, you can view payment
+      />
+      <FullControl
+        smallCover={FullControlFraudMobile}
+        cover={FullControlFraud}
+        subtitle='In the dashboard, you can view payment
         statuses and have the option to delete any report
         if a previously unpaid customer returns and pays on the spot.
         Additionally, you can print out any report as needed.'
-        />
-      </div>
+      />
       <FaqSection />
     </Container>
   );
